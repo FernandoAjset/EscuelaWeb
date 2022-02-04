9659297efc4e41d121f023472e76a75f2f64d496
FerAjset-G3 <fernando_ajset@hotmail.com>
2022-02-04 17:03:05 -0600

    Agregar archivos de proyecto.

---
+++ EscuelaWeb/Controllers/EscuelaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscuelaWeb.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        // GET: EscuelaController
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EscuelaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EscuelaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
